b892727eb44393b1c35ccd39a52018cd1ec6e26e
luigi <luigi_lipreri@hotmail.com>
2020-03-04 12:33:22 -0300

    modificacion tamaño imagenes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 let $primeraCarta = null;
-
+let totalIntentos = 0;
 const $front = document.querySelectorAll(".carta img")
+const $timer = document.querySelector('.temporizador');
+
+let min = 0,
+    sec = 0;
+    hora = 0;
+
+let timer;
+var interval = -1;
+
 const TOTAL_CARTAS = 15;
 let array_imagenes=
   ["img/1.jpg",
@@ -26,20 +35,46 @@ let array_imagenes=
 const $btnEmpezar = document.querySelector("#btn-empezar");
 const $tablero = document.querySelector('#tablero');
 
+function setPlayPause()
+{
+  
+  
+    if(document.getElementById("btn-empezar").value === "Reiniciar")
+    {
+        document.getElementById("btn-empezar").value = "Play";
+        document.getElementById("btn-empezar").innerText = "Play";
+        alert("PLAY!");
+        clearInterval(id);
+      
+    }
+    else
+    {
+        document.getElementById("btn-empezar").value = "Reiniciar";
+        document.getElementById("btn-empezar").innerText = "REINICIAR";
+     
+        alert("REINICIAR!");
+      
+    }
+}
 
 $btnEmpezar.onclick = function(){
-
+  setPlayPause();
+  iniciarTemporizador();
+  id = setInterval(iniciarTemporizador,1000);
+  
   cargarImagenesCartas(mezclarImagenes());
   //iniciar timer.
   //desbloquear cartas
   // cargar imagenes al tablero
 
 }
+function cronometro(){
 
+}
 $tablero.onclick = function(e) {
     let $elemento = e.target;
     const idCarta = e.target.getAttribute('id');
-    // let idDeCartaSeleccionada = e.attributes.id.value;
+
    
     if ($elemento.classList.contains('img-fluid')) {
 
@@ -103,4 +138,31 @@ function mezclarImagenes() {
 
   return array_imagenes;    
 
+}
+
+function iniciarTemporizador() {
+  sec++;
+  if (sec <= 60) {
+      if (sec === 60) {
+          sec = 0;
+          min++;
+          if (min === 60) {
+              min = 0;
+              hora++;
+          }
+      }
+  }
+  else {
+      sec = 0;
+  }
+  $timer.textContent = mostrarDosDigitos(min) + ":" + mostrarDosDigitos(sec);
+}
+
+function mostrarDosDigitos(numero){
+  if(numero<10){
+      numero = "0" + numero;
+      return numero;
+  }else{
+      return numero;
+  };
 }
