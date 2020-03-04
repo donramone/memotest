6b38b5042adf1d0a730449b66accfee5b8fad608
Luigi <luigi_lipreri@hotmail.com>
2020-03-04 00:35:34 -0300

    Refactorizacion nueva forma de mostrar la cartas

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,106 @@
+let $primeraCarta = null;
 
- console.log("workingggjs!");
+const $front = document.querySelectorAll(".carta img")
+const TOTAL_CARTAS = 15;
+let array_imagenes=
+  ["img/1.jpg",
+	"img/1.jpg",
+	"img/2.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+	"img/3.jpg",
+  "img/4.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/5.jpg",
+	"img/6.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/8.jpg"
+	]
 
+
+
+const $btnEmpezar = document.querySelector("#btn-empezar");
 const $tablero = document.querySelector('#tablero');
 
-$tablero.onclick = function(e) {
-    const $elemento = e.target;
-    if ($elemento.classList.contains('img-fluid')) {
-     
-      console.log("tiene una carta js!");
-      manejarClickCuadro($elemento);
-    }else{
-      console.log("NO TIENE una carta js!");
-    }
+
+$btnEmpezar.onclick = function(){
+
+  cargarImagenesCartas(mezclarImagenes());
+  //iniciar timer.
+  //desbloquear cartas
+  // cargar imagenes al tablero
+
 }
 
-function manejarClickCuadro($carta) {
-  console.log("la carta es: " + $carta.src);
-  console.log($carta.getAttribute('src'));
-  // $carta.style.opacity = '0';
-  
+$tablero.onclick = function(e) {
+    let $elemento = e.target;
+    const idCarta = e.target.getAttribute('id');
+    // let idDeCartaSeleccionada = e.attributes.id.value;
+   
+    if ($elemento.classList.contains('img-fluid')) {
+
+      manejarClickCuadro($elemento,idCarta);
+        // mostrarCarta($elemento, idCarta);
+    }else{
+    //  console.log("NO TIENE una carta js!");
+    }
+
+ //   console.log(mezclarImagenes());
+}
+
+function manejarClickCuadro($carta,idCarta) {
+
   if($carta.getAttribute('src') === 'img/dc.png'){
-    carta.className += "img-fluid";
-    $carta.src="img/lantern.png";
+
+    mostrarCarta($carta, idCarta);
+ 
   }else{
     
     $carta.src= 'img/dc.png';
   }
-    // mostrarCuadro($cuadroActual);
-  
-    // if ($primerCuadro === null) {
-    //   $primerCuadro = $cuadroActual;
-    // } else {
-  
-    //   if ($primerCuadro === $cuadroActual) {
-    //     return;
-    //   }
-  
-    //   turnos++;
-  
-    //   if (cuadrosSonIguales($primerCuadro, $cuadroActual)) {
-    //     eliminarCuadro($primerCuadro);
-    //     eliminarCuadro($cuadroActual);
-    //   } else {
-    //     ocultarCuadro($primerCuadro);
-    //     ocultarCuadro($cuadroActual);
-    //   }
-    //   $primerCuadro = null;
-    // }
+
+}
+
+function mostrarCarta($cartaActual, id){
+//console.log($cartaActual);
+
+  $cartaActual.src = array_imagenes[id];
+  if($primeraCarta ===null) {
+    $primeraCarta = $cartaActual;
+
+  }else ($primeraCarta ===$cartaActual)
+  {
+    return;
   }
+  //turnos++
+
+
+
+  //$primeraCarta =null
+}
   
+function cargarImagenesCartas(array_desordenado){
+  for (let i = 0; i < array_desordenado.length ; i++) {
+   // $front[i].src =array_imagenes[i];
+  
+  }
+}
+
+function mezclarImagenes() {
+
+  let i, j, temp;
+  for (i = array_imagenes.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array_imagenes[i];
+    array_imagenes[i] = array_imagenes[j];
+    array_imagenes[j] = temp;
+   
+  }
+
+  return array_imagenes;    
+
+}
