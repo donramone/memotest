2e3414daab8e0134bed1b28d100f980e879132c7
Luigi <luigi_lipreri@hotmail.com>
2020-03-12 18:59:26 -0300

    Lanzamiento version Beta

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 
 let totalIntentos = 0, totalCorrectos = 0;
 let min = 0, sec = 0, cronometro = 0;
-let primeraCarta= null, segundaCarta= null;
-let primeraSeleccion=null, segundaSeleccion=null;
+let primeraCarta = null, segundaCarta = null;
+let primeraSeleccion = null, segundaSeleccion = null;
 
-let array_imagenes=["img/1.jpg","img/1.jpg","img/2.jpg","img/2.jpg","img/3.jpg","img/3.jpg","img/4.jpg","img/4.jpg",
-        "img/5.jpg","img/5.jpg","img/6.jpg","img/6.jpg","img/7.jpg","img/7.jpg","img/8.jpg","img/8.jpg"];
+let array_imagenes = ["img/1.jpg", "img/1.jpg", "img/2.jpg", "img/2.jpg", "img/3.jpg", "img/3.jpg", "img/4.jpg", "img/4.jpg",
+  "img/5.jpg", "img/5.jpg", "img/6.jpg", "img/6.jpg", "img/7.jpg", "img/7.jpg", "img/8.jpg", "img/8.jpg"];
 
 const $carta = document.querySelectorAll(".carta img")
 const $cronometro = document.querySelector('#cronometro');
@@ -15,139 +15,138 @@ const $movimientos = document.querySelector("#movimientos");
 const $msgGanador = document.querySelector("#mensaje-ganador");
 
 
-$btnJugar.onclick = function(){
-  if(document.getElementById("btn-jugar").value === "Parar"){
-    parar();    
+$btnJugar.onclick = function () {
+  if (document.getElementById("btn-jugar").value === "Parar") {
+    parar();
   }
-  else{
+  else {
     jugar();
   }
 }
 
-function parar(){
+function parar() {
   document.getElementById("btn-jugar").value = "Jugar";
   document.getElementById("btn-jugar").innerText = "Jugar";
   pararCronometro();
   ocultarTodasLasCartas();
-  totalIntentos=0;
+  totalIntentos = 0;
   actualizarMovimientos(totalIntentos);
 }
 
-function jugar(){
+function jugar() {
   document.getElementById("btn-jugar").value = "Parar";
   document.getElementById("btn-jugar").innerText = "Parar";
   mostrarMsjGanador("oculto");
-  cronometro = setInterval(iniciarCronometro,1000);
+  cronometro = setInterval(iniciarCronometro, 1000);
   mezclarImagenes();
 }
 
 
-$tableroCartas.onclick = function(e) {
-    let $elemento = e.target;
-    if ($elemento.classList.contains('img-fluid')) {
-      manejarCartaSeleccionada($elemento);
-    }
+$tableroCartas.onclick = function (e) {
+  let $elemento = e.target;
+  if ($elemento.classList.contains('img-fluid')) {
+    manejarCartaSeleccionada($elemento);
+  }
 }
 
 
 function manejarCartaSeleccionada($cartaSeleccionada) {
 
-  if($cartaSeleccionada.getAttribute('src') === 'img/dc.png'){
-    if(primeraSeleccion === null) {
+  if ($cartaSeleccionada.getAttribute('src') === 'img/dc.png') {
+    if (primeraSeleccion === null) {
       primeraCarta = $cartaSeleccionada;
-      primeraSeleccion= array_imagenes[$cartaSeleccionada.getAttribute('id')];
-      mostrarCarta(primeraCarta,primeraSeleccion);
-   
-    }else if(segundaSeleccion === null){
+      primeraSeleccion = array_imagenes[$cartaSeleccionada.getAttribute('id')];
+      mostrarCarta(primeraCarta, primeraSeleccion);
+
+    } else if (segundaSeleccion === null) {
       segundaCarta = $cartaSeleccionada;
-      segundaSeleccion= array_imagenes[$cartaSeleccionada.getAttribute('id')];
-      mostrarCarta(segundaCarta,segundaSeleccion);
+      segundaSeleccion = array_imagenes[$cartaSeleccionada.getAttribute('id')];
+      mostrarCarta(segundaCarta, segundaSeleccion);
       totalIntentos++;
       actualizarMovimientos(totalIntentos);
     }
   }
-  
-  if(segundaSeleccion !== null){
-    if(compararCarta(primeraSeleccion,segundaSeleccion)){
-      ocultarCarta(primeraCarta,segundaCarta);
-    }else if(totalCorrectos===8){
-     mostrarMsjGanador("visible"); 
-     parar();
+
+  if (segundaSeleccion !== null) {
+    if (compararCarta(primeraSeleccion, segundaSeleccion)) {
+      ocultarCarta(primeraCarta, segundaCarta);
+    } else if (totalCorrectos === 8) {
+      mostrarMsjGanador("visible");
+      parar();
     }
     primeraSeleccion = null;
     segundaSeleccion = null;
   }
 }
 
-function compararCarta(carta1, carta2){
+function compararCarta(carta1, carta2) {
 
-    if(carta1 === carta2){
-      totalCorrectos++;
-      return false;
-    }else{
-      return true;
-    }
+  if (carta1 === carta2) {
+    totalCorrectos++;
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function ocultarCarta(carta1,carta2){
+function ocultarCarta(carta1, carta2) {
   setTimeout(function () {
-  carta1.src= 'img/dc.png';
-  carta2.src= 'img/dc.png';
+    carta1.src = 'img/dc.png';
+    carta2.src = 'img/dc.png';
   }, 750);
 
 }
 
-function mostrarCarta(carta,imagen){
-   carta.src = imagen;
+function mostrarCarta(carta, imagen) {
+  carta.src = imagen;
 }
 
-function ocultarTodasLasCartas(){
-  for (let i = 0; i < array_imagenes.length ; i++) {
-      $carta[i].src = 'img/dc.png';
+function ocultarTodasLasCartas() {
+  for (let i = 0; i < array_imagenes.length; i++) {
+    $carta[i].src = 'img/dc.png';
   }
 }
 
 function mezclarImagenes() {
   let i, j, temp;
-  
-   for (i = array_imagenes.length - 1; i > 0; i--) {
+
+  for (i = array_imagenes.length - 1; i > 0; i--) {
 
     j = Math.floor(Math.random() * (i + 1));
     temp = array_imagenes[i];
     array_imagenes[i] = array_imagenes[j];
     array_imagenes[j] = temp;
   }
-  return array_imagenes;    
+  return array_imagenes;
 }
 
 function iniciarCronometro() {
   let secFormat, minFormat;
   sec++;
 
-  if (sec === 60){sec = 0;min++;}
-  
-  if (sec<10){secFormat="0"+sec;}else{secFormat=sec;}
-  if (min<10){minFormat="0"+min;}else{minFormat=min;}
-  
+  if (sec === 60) { sec = 0; min++; }
+
+  if (sec < 10) { secFormat = "0" + sec; } else { secFormat = sec; }
+  if (min < 10) { minFormat = "0" + min; } else { minFormat = min; }
+
   $cronometro.textContent = minFormat + ":" + secFormat;
 }
 
-function pararCronometro(){
-  sec=0;
-  min=0;
+function pararCronometro() {
+  sec = 0;
+  min = 0;
   clearInterval(cronometro);
-  $cronometro.textContent= "00:00";
+  $cronometro.textContent = "00:00";
 }
 
-function actualizarMovimientos(intentos){
-  $movimientos.innerText=intentos;
+function actualizarMovimientos(intentos) {
+  $movimientos.innerText = intentos;
 }
 
-function mostrarMsjGanador(msg){
-  console.log("el mensaje es " + msg);
-  if (msg="oculto"){
-    $msgGanador.className="oculto";
-  }else {
-    $msgGanador.className= "";
+function mostrarMsjGanador(msg) {
+  if (msg === "oculto") {
+    $msgGanador.className = "oculto";
+  } else {
+    $msgGanador.className = "";
   }
 }
